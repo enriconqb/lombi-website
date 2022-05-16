f0cf232d935d9f888a3c7d050e1beb852e8e5a50
Ayesha Aprilia <echa100401@gmail.com>
2022-05-16 10:03:08 +0700

    benerin dari email jadi uname

--- Back-End/ci4rpl/app/Models/AuthModel.php
+++ Back-End/ci4rpl/app/Models/AuthModel.php
@@ -4,9 +4,9 @@ use CodeIgniter\Model;
 
 class AuthModel extends Model
 {
-    function get_data_login($email,$tbl){
+    function get_data_login($username,$tbl){
         $builder = $this->db->table($tbl);
-        $builder->where('email',$email);
+        $builder->where('username',$username);
         $log = $builder->get()->getRow();
         return $log;
     }
