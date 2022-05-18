0fd726e1f7cace66a9adf64e4e896a79827069a3
coco_github <enriconabilqois.budisantoso@gmail.com>
2022-05-18 19:48:12 +0700

    nambah 3 halaman daftar final

--- Back-End/ci4rpl/app/Config/Routes.php
+++ Back-End/ci4rpl/app/Config/Routes.php
@@ -33,6 +33,8 @@ $routes->setAutoRoute(true);
 // route since we don't have to scan directories.
 $routes->get('/', 'Home::index');
 $routes->get('/tentang', 'Tentang::index');
+$routes->get('/daftarlombafinal', 'DaftarLombaFinal::index');
+$routes->get('/kumpulberkasfinal', 'DaftarLombaFinal::kumpulberkasfinal');
 
 /*
  * --------------------------------------------------------------------
