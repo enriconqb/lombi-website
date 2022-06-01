27267c05424f5d3f07667172fa1f809541edab3c
coco_github <enriconabilqois.budisantoso@gmail.com>
2022-06-01 16:41:03 +0700

    fixed error tampilan panitia lomba

--- Back-End/ci4rpl/app/Views/admin/v_admin.php
+++ Back-End/ci4rpl/app/Views/admin/v_admin.php
@@ -31,65 +31,43 @@
           </div>
           <!-- /.card-header -->
           <div class="card-body table-responsive p-0">
-            <table class="table table-hover text-nowrap">
-              <thead>
-                <tr>
-                  <th scope="col">No</th>
-                  <th scope="col">Nama Lomba</th>
-                  <th scope="col">Action</th>
-                </tr>
-              </thead>
-              <tbody>
-              <?php 
-              $i=0; foreach($lomba as $dt){?>
-                <tr>
-                  <th scope=row><?php echo$i+=1;?></th>
-                  <td><?php echo $dt['nama_lomba']?></td>
-                  <td>
-                    <a href="<?php echo base_url('admin/select'); ?><?php echo '/'.$dt['id_lomba']; ?>"><button type="button" class="btn btn-success me-3">Select</button></a>
-                    <a href="<?php echo base_url('admin/edit'); ?><?php echo '/'.$dt['id_lomba']; ?>"><button type="button" class="btn btn-warning me-3">Edit</button></a>
-                  </td>
-                </tr>
-              <?php
-              }?>
-              </tbody>
-            </table>
+          <table class="table table-hover border border-5 col-lg-12">
+            <thead>
+              <tr>
+                <th scope="col">No</th>
+                <th scope="col">Nama Lomba</th>
+                <th scope="col">Action</th>
+              </tr>
+            </thead>
+            <tbody>
+            <?php
+            if ($lomba !== NULL){ 
+            $i=0; foreach($lomba as $dt){?>
+              <tr>
+                <th scope=row><?php echo$i+=1;?></th>
+                <td><?php echo $dt['nama_lomba']?></td>
+                <td>
+                  <a href="<?php echo base_url('admin/select'); ?><?php echo '/'.$dt['id_lomba']; ?>"><button type="button" class="btn btn-success me-3">Select</button></a>
+                  <a href="<?php echo base_url('admin/edit'); ?><?php echo '/'.$dt['id_lomba']; ?>"><button type="button" class="btn btn-warning me-3">Edit</button></a>
+                  <a href="<?php echo base_url('admin/delete'); ?><?php echo '/'.$dt['id_lomba']; ?>"><button type="button" class="btn btn-danger me-3">Delete</button></a>
+                </td>
+              </tr>
+            <?php
+            }} else { ?>
+            <tr>
+              <td colspan=3>
+                <h2>Kamu tidak memiliki lomba apapun</h2>
+              </td>
+            </tr>
+            <?php  
+            }?>
+            </tbody>
+          </table>
           </div>
         </div>
       </div>
       <div class="row">
-        <table class="table table-hover border border-5 col-lg-6">
-          <thead>
-            <tr>
-              <th scope="col">No</th>
-              <th scope="col">Nama Lomba</th>
-              <th scope="col">Action</th>
-            </tr>
-          </thead>
-          <tbody>
-          <?php
-          if ($lomba !== NULL){ 
-          $i=0; foreach($lomba as $dt){?>
-            <tr>
-              <th scope=row><?php echo$i+=1;?></th>
-              <td><?php echo $dt['nama_lomba']?></td>
-              <td>
-                <a href="<?php echo base_url('admin/select'); ?><?php echo '/'.$dt['id_lomba']; ?>"><button type="button" class="btn btn-success me-3">Select</button></a>
-                <a href="#"><button type="button" class="btn btn-warning me-3">Edit</button></a>
-                <a href="<?php echo base_url('admin/delete'); ?><?php echo '/'.$dt['id_lomba']; ?>"><button type="button" class="btn btn-danger me-3">Delete</button></a>
-              </td>
-            </tr>
-          <?php
-          }} else { ?>
-          <tr>
-            <td colspan=3>
-              <h2>Kamu tidak memiliki lomba apapun</h2>
-            </td>
-          </tr>
-          <?php  
-          }?>
-          </tbody>
-        </table>
+        
       </div>
       <!-- /.row -->
     </div>
